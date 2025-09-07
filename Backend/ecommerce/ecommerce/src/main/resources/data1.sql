741a3fd5dd88b1b3372eb04402e173b27ea0ce8c
Sri Mourya <srimouryav@emailcom>
2025-09-07 23:50:00 +0530

    Initial commit – import project

---
+++ Backend/ecommerce/ecommerce/src/main/resources/data1.sql
@@ -0,0 +1,19 @@
+INSERT INTO product
+  (name, `DESC`, brand, price, category, release_date, available, quantity)
+VALUES
+  ('TataNexon',
+   'A compact suv with excellent safetyfeatures',
+   'TataMotors',
+   75000.00,
+   'Cars',
+   '2024-01-24',
+   TRUE,
+   150),
+  ('Hyundaicreata',
+   'A car stylish suv with advanced features and comfortable interiors',
+   'Hyundai',
+   75000.00,
+   'Cars',
+   '2024-01-14',
+   TRUE,
+   150);
